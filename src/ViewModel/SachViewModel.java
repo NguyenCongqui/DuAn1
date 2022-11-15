e5f78f6b63284a303c3aef24bb2b628538006db9
QUAN\quanh <quanlmph23859@fpt.edu.vn>
2022-11-16 00:54:07 +0700

    quan2

---
+++ src/ViewModel/SachViewModel.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ViewModel;
+
+import Utilities.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author quanh
+ */
+public class SachViewModel {
+     private int id;
+    private String tenSach;
+    private String loaiSach;
+    private boolean trangThai;
+
+    public SachViewModel() {
+    }
+
+    public SachViewModel(int id, String tenSach, String loaiSach, boolean trangThai) {
+        this.id = id;
+        this.tenSach = tenSach;
+        this.loaiSach = loaiSach;
+        this.trangThai = trangThai;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public String getLoaiSach() {
+        return loaiSach;
+    }
+
+    public void setLoaiSach(String loaiSach) {
+        this.loaiSach = loaiSach;
+    }
+
+    public boolean isTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(boolean trangThai) {
+        this.trangThai = trangThai;
+    }
+    
+}
